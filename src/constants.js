01aa56e2c3867830d32bef5a7a78ff97640245ca
Erkin Öztürk <erkinozturk@outlook.com.tr>
2024-04-16 02:18:10 +0300

    Done

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  exit: `If you want to exit just close the tab.`,
+  library: `There are all kinds of books here. In addition to books about software, I also love reading fantasy and science fiction books.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch series on Netflix.`,
+  tv: `That's my Android TV.`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  resume: `This is my desk and on it is my resume, <a href="https://github.com/erkinozturk/Resume/blob/main/Erkin-%C3%96zt%C3%BCrk-Resume.pdf" target="_blank">Check it out!</a>`,
+  projects: `This portfolio made with the Kaboom.js library which is a library for making games in JavaScript. Text is rendered with HTML/CSS.`,
+  pc: `This is my PC. I work mostly in React Native these days. I've made a couple of apps in that framework. I also like editing videos.`,
+  bed: `This is my bed. I still can't get enough sleep because I went to bed late,  but I've started working on going to bed early.`,
+}; 
